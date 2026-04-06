5ab09fb294e3245adb9ff46e15469a85e1d516ec
Laurent Toutain <Laurent@Touta.in>
2026-04-06 10:43:04 +0200

    add module and date

--- convert.sh
+++ convert.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <module> <date>"
+  echo "  Example: $0 ietf-schc 2026-04-05"
+  exit 1
+fi
+
+MODULE=$1
+DATE=$2
+BASE="${MODULE}@${DATE}"
 
 git pull
-# generate test sid allocation
-pyang --sid-generate-file=2550:400 --sid-list ietf-schc@2025-11-24.yang 
-uv run python3 csv2sid.py SID-mapping-bis.csv ietf-schc@2025-11-24.sid ietf-schc@2025-11-24-test.sid
 
+pyang --sid-generate-file=2550:400 --sid-list --sid-extension ${BASE}.yang
+uv run python3 csv2sid.py SID-mapping-${DATE}.csv ${BASE}.sid ${BASE}-mapped.sid
 
-pyang --sid-generate-file=2550:400 --sid-list --sid-extension ietf-schc@2025-11-24.yang 
-uv run python3 csv2sid.py SID-mapping-bis.csv ietf-schc@2025-11-24.sid ietf-schc@2025-11-24-ext.sid
-
-git add ietf-schc@2025-11-24-test.sid
-git add ietf-schc@2025-11-24-ext.sid
+git add SID-mapping-${DATE}.xlsx
+git add SID-mapping-${DATE}.csv
+git add ${BASE}.yang
+git add ${BASE}-mapped.sid
 git commit -m "update sid"
 git push
